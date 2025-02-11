7b32b4ffafdaccfb430af1525965cac61730395c
Javiera Izquierdo Martino <javieraizquierdomartino@Ordenador-portatil-de-Javiera.local>
2025-02-11 15:37:06 -0300

    validacion form basico

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6,
+};
 
 @Component({
   selector: 'app-basic-page',
   standalone: false,
   templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
+  styleUrl: './basic-page.component.css',
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+  public myForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      price: [0, [Validators.required, Validators.min(0)]],
+      inStorage: [0, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  ngOnInit(): void {
+    // this.myForm.reset(rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors 
+    && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
 }
