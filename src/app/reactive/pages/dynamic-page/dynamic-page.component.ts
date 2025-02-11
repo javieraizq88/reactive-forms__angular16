0957f6665982e3b70d81e306bb093b3f2e9466ab
Javiera Izquierdo Martino <javieraizquierdomartino@Ordenador-portatil-de-Javiera.local>
2025-02-11 14:41:41 -0300

    sidebar con redireccion a comp

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  standalone: false,
+  templateUrl: './dynamic-page.component.html',
+  styleUrl: './dynamic-page.component.css'
+})
+export class DynamicPageComponent {
+
+}
