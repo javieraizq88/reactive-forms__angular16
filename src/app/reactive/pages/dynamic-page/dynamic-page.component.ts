6256d42c0c074dd6415313b26dcb20076277f390
Javiera Izquierdo Martino <javieraizquierdomartino@Ordenador-portatil-de-Javiera.local>
2025-02-18 11:48:54 -0300

    formularios dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -7,5 +8,84 @@ import { Component } from '@angular/core';
   styleUrl: './dynamic-page.component.css'
 })
 export class DynamicPageComponent {
+
+  public myForm: FormGroup;
+  public newFavorite: FormControl = new FormControl('', Validators.required );
+
+  constructor( private fb: FormBuilder ) { 
+    this.myForm = this.fb.group({ 
+      name: ['', [ Validators.required, Validators.minLength(3) ]],
+      favoriteGames: this.fb.array([
+        ['Metal Gear', Validators.required ],
+        ['Death Stranding', Validators.required ],
+      ])
+    });
+  }
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number ) {
+    return formArray.controls[index].errors
+        && formArray.controls[index].touched;
+  }
+
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+  onAddToFavorites():void {
+
+    if ( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
+
+  }
+
+
+  onDeleteFavorite( index:number ):void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit():void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    // Clear the favoriteGames array without resetting the entire form
+    (this.myForm.get('favoriteGames') as FormArray).clear(); 
+    // this.myForm.reset(); // Uncomment this line to reset the entire form
+
+  }
 
 }
